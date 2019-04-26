5d9256087311c0344cad5a3afc70df4a144d1190
Cynthia <cynthiahuallanca@gmail.com>
2019-04-26 15:17:52 -0400

    final changes to JavaScript project

--- JavaScript/Level 2/static/js/app.js
+++ JavaScript/Level 2/static/js/app.js
@@ -4,10 +4,10 @@ var tableData = data;
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-// Console.log the UFO data from data.js
+// To Debug. Console.log the UFO data from data.js 
 //console.log(data);
 
-// Load data into table --------------
+// -------------- Load data into table --------------
 data.forEach(function(UFO) {
    //console.log(UFO);
   var row = tbody.append("tr");
@@ -20,12 +20,11 @@ data.forEach(function(UFO) {
      cell.text(value);
   });
 });
-// End--------------------------------
+// ----------------------- End ----------------------
 
 
 
-// Filter by DateTime ----------------
-
+// -------------- Filter by DateTime ----------------
 // Define button 
 var button = d3.select("#filter-btn-datetime");
 
@@ -33,91 +32,157 @@ var button = d3.select("#filter-btn-datetime");
  button.on("click", function() {
   d3.event.preventDefault();
   console.log("Hi, a button was clicked!");
-  var inputElement = d3.select("#datetime");//define input element
+  var inputElement = d3.select("#datetime"); // Defining input element
   var inputValue = inputElement.property("value"); // Get the value property of the input element 
-  console.log(inputValue); // see in the console the input value on the filter  
-  console.log(tableData); // va a mostrar todos los datos, no se para que sirve aun 
+  console.log(inputValue); // See in the console the input value on the filter  
+  // console.log(tableData); // To Debug  
+  var removeBool = true;
 
   var filteredData = tableData.filter(record => record.datetime === inputValue);
   console.log(filteredData); // this is working. it's showing the filtered data in the log! :) yuhu
 
+   // Debug purpose
+  //console.log(filteredData); 
+
+  // Define lenght (to use it below with if statement)
+  var filteredDatalength = filteredData.length; 
+
+  // Defining Alert when no data is found
+  function myFunction() {
+  alert("There's no data with your filter, Try again!");
+  removeBool = false;
+  }
+
+  // Pop up alert when no data is found
+  if (filteredDatalength === 0 ) {
+    console.log("There's no data with your filter, Try again");
+    myFunction();
+  };
+
   // clear table to show the filtered data after
+  if(removeBool === true){
   d3.select('tbody').selectAll('tr').remove();
+  }
   
-  //TODO: improve by storing the function in a variable
-  // to show the filtered data
+  // To show the filtered data
   filteredData.forEach(function(UFO) {
     //console.log(UFO);
     var row = tbody.append("tr");
     Object.entries(UFO).forEach(function([key, value]) {
+      // To Debug
       //console.log(key, value);
-      // Append a cell to the row for each value
-      // in the UFO report object
+      // Append a cell to the row for each value in the UFO report object
       var cell = row.append("td");
       cell.text(value);
     });
   });
-
-  //TODO: cuando filtro nada debe haber un mensaje
 });
-// End--------------------------------
+// ----------------------- End ----------------------
 
-
-
-// Filter by City ----------------
-// TODO: lowercase input value 
+// ---------------- Filter by City ----------------
+// Define button 
 var button = d3.select("#filter-btn-city");
 
 // Function to make button filter
  button.on("click", function() {
   d3.event.preventDefault();
   console.log("Hi, button to filter city was clicked!");
-  var inputElement = d3.select("#city");//define input element
-  var inputValue = inputElement.property("value"); // Get the value property of the input element 
-  console.log(inputValue); // see in the console the input value on the filter  
-  console.log(tableData); // va a mostrar todos los datos, no se para que sirve aun 
+  // Defining input element
+  var inputElement = d3.select("#city"); 
 
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value").toLowerCase();  
+  
+  // See in the console the input value on the filter
+  console.log(inputValue);   
+
+  var removeBool = true;
+
+  // Show the filtered data in the log! 
   var filteredData = tableData.filter(record => record.city === inputValue);
-  console.log(filteredData); // this is working. it's showing the filtered data in the log! :) yuhu
+  console.log(filteredData); 
+  
+  // To Debug
+  //console.log(filteredData); 
+
+  // Define lenght (to use it below with if statement)
+  var filteredDatalength = filteredData.length; 
+
+  // Defining Alert when no data is found
+  function myFunction() {
+  alert("There's no data with your filter, Try again!");
+  removeBool = false;
+  }
+
+  // Pop up alert when no data is found
+  if (filteredDatalength === 0 ) {
+    console.log("There's no data with your filter, Try again");
+    myFunction();
+  };
 
   // clear table to show the filtered data after
+  if(removeBool === true){
   d3.select('tbody').selectAll('tr').remove();
+  }
   
-  //TODO: improve by storing the function in a variable
-  // to show the filtered data
+  // Show the filtered data
   filteredData.forEach(function(UFO) {
     //console.log(UFO);
     var row = tbody.append("tr");
     Object.entries(UFO).forEach(function([key, value]) {
       //console.log(key, value);
-      // Append a cell to the row for each value
-      // in the UFO report object
+      // Append a cell to the row for each value in the UFO report object
       var cell = row.append("td");
       cell.text(value);
     });
   });
 });
-// End--------------------------------
+// ----------------------- End ----------------------
 
-// Filter by State ----------------
-// TODO: lowercase input value 
+// ---------------- Filter by State ----------------
 var button = d3.select("#filter-btn-state");
 
 // Function to make button filter
  button.on("click", function() {
   d3.event.preventDefault();
   console.log("Hi, button to filter state was clicked!");
-  var inputElement = d3.select("#state");//define input element
-  var inputValue = inputElement.property("value"); // Get the value property of the input element 
-  console.log(inputValue); // see in the console the input value on the filter  
-  console.log(tableData); // va a mostrar todos los datos, no se para que sirve aun 
 
+  // Define input element
+  var inputElement = d3.select("#state");
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value").toLowerCase();  
+  // see in the console the input value on the filter
+  console.log(inputValue);   
+  // To Debug 
+  console.log(tableData);  
+  
+  var removeBool = true;
+
+  // Defining filtered data 
   var filteredData = tableData.filter(record => record.state === inputValue);
-  console.log(filteredData); // this is working. it's showing the filtered data in the log! :) yuhu
+  
+  // Debug purpose
+  //console.log(filteredData); 
+
+  // Define lenght (to use it below with if statement)
+  var filteredDatalength = filteredData.length; 
+
+  // Defining Alert when no data is found
+  function myFunction() {
+  alert("There's no data with your filter, Try again!");
+  removeBool = false;
+  }
+
+  // Pop up alert when no data is found
+  if (filteredDatalength === 0 ) {
+    console.log("There's no data with your filter, Try again");
+    myFunction();
+  };
 
   // clear table to show the filtered data after
+  if(removeBool === true){
   d3.select('tbody').selectAll('tr').remove();
-  
+  }
   //TODO: improve by storing the function in a variable
   // to show the filtered data
   filteredData.forEach(function(UFO) {
@@ -125,45 +190,67 @@ var button = d3.select("#filter-btn-state");
     var row = tbody.append("tr");
     Object.entries(UFO).forEach(function([key, value]) {
       //console.log(key, value);
-      // Append a cell to the row for each value
-      // in the UFO report object
+      // Append a cell to the row for each value in the UFO report object
       var cell = row.append("td");
       cell.text(value);
     });
   });
 });
-// End--------------------------------
+// ----------------------- End ----------------------
 
-// Filter by Shape ----------------
-// TODO: lowercase input value 
+// ---------------- Filter by Shape ----------------
 var button = d3.select("#filter-btn-shape");
 
 // Function to make button filter
  button.on("click", function() {
   d3.event.preventDefault();
   console.log("Hi, button to filter shape was clicked!");
-  var inputElement = d3.select("#shape");//define input element
-  var inputValue = inputElement.property("value"); // Get the value property of the input element 
-  console.log(inputValue); // see in the console the input value on the filter  
-  console.log(tableData); // va a mostrar todos los datos, no se para que sirve aun 
+  // define input element
+  var inputElement = d3.select("#shape");
+   // Get the value property of the input element 
+  var inputValue = inputElement.property("value").toLowerCase();
+  // see in the console the input value on the filter
+  console.log(inputValue);   
+   // va a mostrar todos los datos, no se para que sirve aun
+  console.log(tableData); 
+  var removeBool = true;
 
   var filteredData = tableData.filter(record => record.shape === inputValue);
-  console.log(filteredData); // this is working. it's showing the filtered data in the log! :) yuhu
+  console.log(filteredData);
+
+  // Debug purpose
+  //console.log(filteredData); 
+
+  // Define lenght (to use it below with if statement)
+  var filteredDatalength = filteredData.length; 
+
+  // Defining Alert when no data is found
+  function myFunction() {
+  alert("There's no data with your filter, Try again!");
+  removeBool = false;
+  }
+
+  // Pop up alert when no data is found
+  if (filteredDatalength === 0 ) {
+    console.log("There's no data with your filter, Try again");
+    myFunction();
+  };
 
   // clear table to show the filtered data after
+  if(removeBool === true){
   d3.select('tbody').selectAll('tr').remove();
-  
-  //TODO: improve by storing the function in a variable
-  // to show the filtered data
+  }
+
+  // to show the filtered data (tHis can be improved by defining the function in first place )
   filteredData.forEach(function(UFO) {
     //console.log(UFO);
     var row = tbody.append("tr");
     Object.entries(UFO).forEach(function([key, value]) {
       //console.log(key, value);
-      // Append a cell to the row for each value
-      // in the UFO report object
+      // Append a cell to the row for each value in the UFO report object
       var cell = row.append("td");
       cell.text(value);
     });
   });
 });
+// ----------------------- End ----------------------
